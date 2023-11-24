558ef62844ea30b7c9df3d028ef8f7a7969f4740
nabelly <anabellymontibeller@gmail.com>
2023-11-24 11:08:17 -0300

    starting principal Frontend

--- FrontEnd/nomeApp/App.js
+++ FrontEnd/nomeApp/App.js
@@ -1,34 +1,124 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
-import { NavigationContainer, TabActions } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator} from '@react-navigation/stack';
-import AdmPage from  './src/views/admPage';
-import UserPage from './src/views/userPage';
-import HomePage from './src/views/home';
-import Welcome from './src/views/welcome';
-import loginUser from './src/views/Login/loginuser';
-
-import Financeiro from './src/views/AdmAssets/Financeiro';
-import CadNovo from './src/views/AdmAssets/NovoAp';
-import Painel from './src/views/AdmAssets/Painel';
-import Blocos from './src/views/AdmAssets/Blocos';
-import Vagas from './src/views/AdmAssets/Vagas';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { Dimensions } from 'react-native';
+import * as Animatable from 'react-native-animatable'
 
 export default function App() {
+  
+  
 
-  const Tab = createBottomTabNavigator();
-  const Stack = createStackNavigator();
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name = "Welcome" component={Welcome} options={{headerShown: false}}/>
-        <Stack.Screen name = "HomePage" component={HomePage} options={{headerShown: false}}/>
-        <Stack.Screen name = "LoginUser" component={loginUser} options={{headerShown: false}}/>
-        <Stack.Screen name = "AdmPage" component={AdmPage} options={{headerShown: false}}/>
-        <Stack.Screen name = "UserPage" component={UserPage} options={{headerShown: false}}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+    <View style={styles.container}>
+        <View style={styles.row}>
+          <TouchableOpacity
+              style={styles.btnNavegation}
+            >
+              <Text  style={styles.btnNavText}>Financeiro</Text>
+            </TouchableOpacity>
+
+          <TouchableOpacity
+              style={styles.btnNavegation}
+          >
+              <Text  style={styles.btnNavText}>Novo Cadastro</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+              style={styles.btnNavegation}
+          >
+              <Text  style={styles.btnNavText}>Info</Text>
+          </TouchableOpacity>
+
+        </View>
+
+      <View style={styles.btnContainer}>
+          <TouchableOpacity
+            style={styles.button}
+          >
+            <Text  style={styles.buttonText}>BLOCOS</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.button}
+          >
+            <Text  style={styles.buttonText}>VAGAS</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.button}
+          >
+            <Text  style={styles.buttonText}>AGENDAMENTOS</Text>
+          </TouchableOpacity>
+        </View>
+    </View>
+    </>
   );
 }
+let screenWidth = Dimensions.get('window').width;
+const styles = StyleSheet.create({
+  
+  container: {
+    flex: 1,
+    backgroundColor: '#5D576B',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  row: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    flexDirection: 'row'
+  },
+
+  btnNavegation: {
+    backgroundColor: '#4A90E2', 
+    justifyContent:'center',
+    alignItems: 'center',
+    padding: 10,
+    width: screenWidth * 0.25, 
+    height: 50,
+    borderRadius: 10,
+  },
+
+  btnNavText: {
+    padding: '2%',
+    color: '#FFF',
+    fontWeight: 'bold',
+    display: 'flex',
+    justifyContent: 'center'
+  },
+
+  btnContainer: {
+    marginTop: '30%'
+  },
+
+  button: {
+    backgroundColor: "#a1a1a1",
+    display: 'flex',
+    justifyContent:'center',
+    alignItems: 'center',
+    marginBottom: '10%',
+    width: screenWidth * 0.90,
+    height: '50px',
+    borderRadius: '10px',
+  },
+
+  buttonText: {
+    padding: '2%',
+    color: '#FFF',
+    fontWeight: 'bold',
+    display: 'flex',
+    justifyContent: 'center'
+  }
+
+
+
+
+
+
+
+
+
+
+});
--- FrontEnd/nomeApp/copyapp.js
+++ FrontEnd/nomeApp/copyapp.js
@@ -45,4 +45,41 @@ export default function App() {
 
     </NavigationContainer>
   );
-}
+ }
+
+
+// import React from 'react';
+// import { StatusBar } from 'react-native';
+// import { NavigationContainer, TabActions } from '@react-navigation/native';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { createStackNavigator} from '@react-navigation/stack';
+// import AdmPage from  './src/views/admPage';
+// import UserPage from './src/views/userPage';
+// import HomePage from './src/views/home';
+// import Welcome from './src/views/welcome';
+// import loginUser from './src/views/Login/loginuser';
+
+// import Financeiro from './src/views/AdmAssets/Financeiro';
+// import CadNovo from './src/views/AdmAssets/NovoAp';
+// import Painel from './src/views/AdmAssets/Painel';
+// import Blocos from './src/views/AdmAssets/Blocos';
+// import Vagas from './src/views/AdmAssets/Vagas';
+
+// export default function App() {
+
+//   const Tab = createBottomTabNavigator();
+//   const Stack = createStackNavigator();
+
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name = "Welcome" component={Welcome} options={{headerShown: false}}/>
+//         <Stack.Screen name = "HomePage" component={HomePage} options={{headerShown: false}}/>
+//         <Stack.Screen name = "LoginUser" component={loginUser} options={{headerShown: false}}/>
+//         <Stack.Screen name = "AdmPage" component={AdmPage} options={{headerShown: false}}/>
+//         <Stack.Screen name = "UserPage" component={UserPage} options={{headerShown: false}}/>
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
